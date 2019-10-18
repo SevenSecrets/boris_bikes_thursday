085eb44eb512c9775a5febef8fabb9e5ef3fa1de
ellieSMASH <eliseaston@gmail.com>
2019-10-18 14:59:26 +0100

    Added report broken bike feature

--- spec/bike_spec.rb
+++ spec/bike_spec.rb
@@ -3,7 +3,12 @@ require "Bike"
 describe Bike do
   test_bike = Bike.new
   it "test bike has working? method" do
-    expect(test_bike).to respond_to(:working?)
+    expect(test_bike).to be_working
+  end
+
+  it "can be reported broken" do
+    test_bike.report_broken
+    expect(test_bike).to be_broken
   end
 
 end
